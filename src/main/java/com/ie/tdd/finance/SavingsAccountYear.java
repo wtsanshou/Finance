33dbbbab2c7583b155fed2538454a86cd105bb2f
wtsanshou <158300054@qq.com>
2018-03-31 15:08:10 +0100

    The class from James Shore 4

--- src/main/java/com/ie/tdd/finance/SavingsAccountYear.java
+++ src/main/java/com/ie/tdd/finance/SavingsAccountYear.java
@@ -3,6 +3,7 @@ package com.ie.tdd.finance;
 public class SavingsAccountYear {
 
     private int startingBalance = 0;
+    private int capitalGain = 0;
     private int interestRate = 0;
 
     public SavingsAccountYear() {
@@ -13,31 +14,29 @@ public class SavingsAccountYear {
         this.interestRate = interestRate;
     }
 
+    public SavingsAccountYear(int startingBalance, int capitalGain, int interestRate) {
+        this.startingBalance = startingBalance;
+        this.capitalGain = capitalGain;
+        this.interestRate = interestRate;
+    }
+
     public int startingBalance() {
         return startingBalance;
     }
 
-    public void deposit(int amount) {
-        startingBalance += amount;
+    public int interestRate() {
+        return interestRate;
     }
 
-    public int balance() {
-        return startingBalance;
+    public int endingBalance() {
+        return startingBalance() + (startingBalance() * interestRate / 100);
     }
 
-    public void withdrawal(int amount) {
+    public void withdraw(int amount) {
         startingBalance -= amount;
     }
 
     public SavingsAccountYear nextYear() {
         return new SavingsAccountYear(this.endingBalance(), interestRate);
-    }
-
-    public int endingBalance() {
-        return balance() + (balance() * interestRate / 100);
-    }
-
-    public int interestRate() {
-        return interestRate;
     }
 }
